b02b6a963d378425969a31208033f1129b6ee8ce
ZbyszekAdamek <zbigniew.t.adamek@gmail.com>
2022-04-27 13:12:07 +0200

    Initial commit

---
+++ src/main/java/com/example/shop/AppConfig.java
@@ -0,0 +1,54 @@
+package com.example.shop;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.LocaleContextResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+
+import javax.validation.Validator;
+import java.util.Locale;
+
+
+@Configuration
+@ComponentScan("com.example.shop")
+@EnableTransactionManagement
+public class AppConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:validationMessages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+    @Bean
+    public Validator validator() {
+        LocalValidatorFactoryBean lvfb = new LocalValidatorFactoryBean();
+        lvfb.setValidationMessageSource(messageSource());
+        return lvfb;
+    }
+    @Bean(name="localeResolver")
+    public LocaleContextResolver getLocaleContextResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("pl","PL"));
+        return localeResolver;
+    }
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setForceEncoding(true);
+        characterEncodingFilter.setEncoding("UTF-8");
+        registrationBean.setFilter(characterEncodingFilter);
+        return registrationBean;
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/home.jsp
@@ -0,0 +1,18 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<!DOCTYPE html>
+<html lang="pl-PL">
+
+<head>
+
+
+    <title>Online shop</title>
+
+
+</head>
+<body>
+Witam na stronie.
+
+</body>
+</html>
